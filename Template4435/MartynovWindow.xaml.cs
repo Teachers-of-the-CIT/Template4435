537b3739b49bf439df771bd31e57f8aaf186213a
Максим Мартынов <martynov.max-333@yandex.ru>
2022-10-13 13:37:36 +0300

    Done laba2

--- Template4435/MartynovWindow.xaml.cs
+++ Template4435/MartynovWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -33,8 +34,8 @@ namespace Template4435
             OpenFileDialog ofd = new OpenFileDialog()
             {
                 DefaultExt = "*.xls;*.xlsx",
-                Filter = "файл Excel (5.xlsx)|*.xlsx",
-                Title = "Выберите файл базы данных"
+                Filter = "файлы Excel |*.xlsx",
+                Title = "Выберите Excel файл"
             };
             if (!(ofd.ShowDialog() == true))
                 return;
@@ -55,14 +56,67 @@ namespace Template4435
 
             for (int i = 1; i < rows; i++)
             {
-                UsersEntities.GetContext().Users.Add(new User() { Role = list[i, 0],  FIO = list[i, 1], Login = list[i, 2], Password = list[i, 3]});
+                UsersEntities.GetContext().Users
+                    .Add(new User() { Role = list[i, 0],  FIO = list[i, 1], Login = list[i, 2], Password = list[i, 3]});
             }
             UsersEntities.GetContext().SaveChanges();
         }
 
         private void ExportBtn_Click(object sender, RoutedEventArgs e)
         {
-            var role = UsersEntities.GetContext().Users.ToList().OrderBy(u => u.Role).ToList();
+            var roles = UsersEntities.GetContext().Users
+                .ToList()
+                .GroupBy(r => r.Role)
+                .ToList();
+            var users = UsersEntities.GetContext().Users
+                .ToList()
+                .OrderBy(u => u.Role)
+                .ToList();
+
+            var app = new Excel.Application();
+            app.SheetsInNewWorkbook = roles.Count();
+            Excel.Workbook workbook = app.Workbooks.Add(Type.Missing);
+
+            for (int i = 0; i < roles.Count(); i++)
+            {
+                int startRowIndex = 2;
+                Excel.Worksheet worksheet = app.Worksheets.Item[i + 1];
+                worksheet.Name = roles[i].Key.ToString();
+                worksheet.Cells[1][startRowIndex] = "Логин";
+                worksheet.Cells[2][startRowIndex] = "Пароль";
+                startRowIndex++;
+                foreach (var user in users)
+                {
+                    if (roles[i].Key == user.Role)
+                    {
+                        Excel.Range headerRange = worksheet.Range[worksheet.Cells[1][1], worksheet.Cells[2][1]];
+                        headerRange.Merge();
+                        headerRange.Value = roles[i].Key;
+                        headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                        headerRange.Font.Bold = true;
+                        worksheet.Cells[1][startRowIndex] = user.Login;
+                        worksheet.Cells[2][startRowIndex] = GetHash(user.Password);
+                        startRowIndex++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                    Excel.Range rangeBorders = worksheet.Range[worksheet.Cells[1][1], worksheet.Cells[2][startRowIndex - 1]];
+                    rangeBorders.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                    worksheet.Columns.AutoFit();
+                }
+                app.Visible = true;
+            }  
+        }
+
+        public string GetHash(string input)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            return Convert.ToBase64String(hash);
         }
     }
 }
