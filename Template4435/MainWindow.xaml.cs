1d2d9ba3656e6ee2ba759ae3b29e68f78797d090
disai <di-saifutdinova@bk.ru>
2022-11-20 18:17:28 +0300

    импорт и экспорт данных(Excel)

--- Template4435/MainWindow.xaml.cs
+++ Template4435/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Template4435
 {
@@ -20,58 +22,122 @@ namespace Template4435
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Entities _entities;
         public MainWindow()
         {
             InitializeComponent();
+            _entities = new Entities();
         }
 
-        private void BnTask_Click(object sender, RoutedEventArgs e)
+        private void BnImport_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Автор: Сабиров Зульфат Зуфарович","4435_Сабиров_Зульфат");
-        }
-        private void toWindowBtn_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Мартынов Максим Дмитриевич, 19 лет, группа_4435","4435_Мартынов");
-        }
-        private void AzatBtn_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Автор: Хакимзянов Азат Гайсович", "4435_Хакимзянов_Азат");
-        }
-        private void BnnTask_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Автор: Назмутдинов Рузаль Ильгизович", "4435_Назмутдинов_Рузаль");
-        }
-        private void BtnCHELNY_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("ЕРКАШОВ 4435 19", "4435_ЕРКАШОВ");
-        }
-        private void BtnNikita_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("КРАВЧЕНКО 4435 16", "4435_КРАВЧЕНКО");
-        }
-        private void LR1_Shumilkin_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Автор: Шумилкин Александр Олегович", "4435_Шумилкин_Александр");
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                DefaultExt = "*.xls;*.xlsx",
+                Filter = "файл Excel (Spisok.xlsx)|*.xlsx",
+                Title = "Выберите файл базы данных"
+            };
+            if (!(ofd.ShowDialog() == true))
+                return;
+
+            string[,] list;
+            Excel.Application ObjWorkExcel = new Excel.Application();
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(ofd.FileName);
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            int _columns = (int)lastCell.Column;
+            int _rows = (int)lastCell.Row;
+            list = new string[_rows, _columns];
+            for (int j = 0; j < _columns; j++)
+                for (int i = 0; i < _rows; i++)
+                    list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text;
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+            ObjWorkExcel.Quit();
+            GC.Collect();
+
+            using (Entities entities = new Entities())
+            {
+                for (int i = 1; i < _rows; i++)
+                {
+                    entities.users.Add(new users()
+                    {
+                        Id = list[i, 0],
+                        FIO = list[i, 1],
+                        Login = list[i, 2],
+                        Doljnost = list[i, 3]
+                    });
+                }
+                entities.SaveChanges();
+            }
         }
 
-        private void Maximov_Click(object sender, RoutedEventArgs e)
+        private void BnExport_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Автор: Максимов Роман Сергеевич", "4435_Максимов_Роман");
-        }
-
-        private void Adieva_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Автор: Адиева Айгуль Ринатовна", "4435_Адиева_Айгуль");
-        }
-
-        private void Saifutdinova_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Сайфутдинова Диляра Искадеровна, 19", "4435_Сайфутдинова");
-        }
-
-        private void Safina_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Сафина Яна Робертовна, 19", "4435_Сафина");
+            List<users> allUsers;
+            
+            var allDoljnosti = _entities.users.Select(u => new {Doljnost = u.Doljnost}).Distinct().ToList();
+            allUsers = _entities.users.ToList().OrderBy(g => g.Id).ToList();
+            
+            var app = new Excel.Application();
+            app.SheetsInNewWorkbook = allDoljnosti.Count();
+            Excel.Workbook workbook = app.Workbooks.Add(Type.Missing);
+            for (int i = 0; i < allDoljnosti.Count(); i++)
+            {
+                int startRowIndex = 1;
+                Excel.Worksheet worksheet = app.Worksheets.Item[i + 1];
+                worksheet.Name = Convert.ToString(allDoljnosti[i].Doljnost);
+               
+                    worksheet.Cells[1][2] = "Порядковый номер";
+                    worksheet.Cells[2][2] = "ФИО";
+                    worksheet.Cells[3][2] = "Логин";
+                startRowIndex++;    
+                var usersCategories = allUsers.GroupBy(s => s.Doljnost).ToList();
+                foreach (var users in usersCategories)
+                {
+                    if (users.Key == allDoljnosti[i].Doljnost)
+                    {
+                        Excel.Range headerRange =
+                        worksheet.Range[worksheet.Cells[1][1],
+                        worksheet.Cells[2][1]];
+                        headerRange.Merge();
+                        headerRange.Value = allDoljnosti[i].Doljnost;
+                        headerRange.HorizontalAlignment =
+                        Excel.XlHAlign.xlHAlignCenter;
+                        headerRange.Font.Italic = true;
+                        startRowIndex++;
+                        foreach (users user in allUsers)
+                        {
+                            if (user.Doljnost == users.Key)
+                            {
+                                worksheet.Cells[1][startRowIndex] = user.Id;
+                                worksheet.Cells[2][startRowIndex] = user.FIO;
+                                worksheet.Cells[3][startRowIndex] = user.Login;
+                                startRowIndex++;
+                            }
+                        }
+                        worksheet.Cells[1][startRowIndex].Formula =
+                        $"=СЧЁТ(A3:A{startRowIndex - 1})";
+                        worksheet.Cells[1][startRowIndex].Font.Bold =
+                        true;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                Excel.Range rangeBorders = worksheet.Range[worksheet.Cells[1][1],
+                    worksheet.Cells[3][startRowIndex - 1]];
+                rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle =
+                rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle =
+                rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle =
+                rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle =
+                rangeBorders.Borders[Excel.XlBordersIndex.xlInsideHorizontal].LineStyle =
+                rangeBorders.Borders[Excel.XlBordersIndex.xlInsideVertical].LineStyle =
+                Excel.XlLineStyle.xlContinuous;
+                worksheet.Columns.AutoFit();
+            }
+            app.Visible = true;
+        
         }
     }
 }
